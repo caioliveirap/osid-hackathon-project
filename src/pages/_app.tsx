aaf889233cf17e6338ea187dac973697b1f9d990
caioliveirap <caio191201@gmail.com>
2023-06-17 15:31:55 -0300

    feat: added layout for admin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
+import Admin from '@/layouts/Admin';
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-	<Component {...pageProps} />
+	<Admin>
+		<Component {...pageProps} />
+	</Admin>
 );
 
 export default MyApp;
---
+++ src/pages/admin/dashboard.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import axios from 'axios';
+import Admin from '@/layouts/Admin';
+
+const Dashboard = () => {
+	return (
+		<div className="">
+			<span>teste</span>
+
+			{/* <input
+				type="text"
+				placeholder="email"
+				className="text-black"
+				onChange={(e: any) => {
+					setEmail(e.target.value);
+				}}
+			/>
+			<input
+				type="text"
+				placeholder="password"
+				className="text-black"
+				onChange={(e: any) => {
+					setPassword(e.target.value);
+				}}
+			/> */}
+			{/* <button onClick={signIn}>Teste</button> */}
+		</div>
+	);
+};
+
+Dashboard.layout = Admin;
+export default Dashboard;
